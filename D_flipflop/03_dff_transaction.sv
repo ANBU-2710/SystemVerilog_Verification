739d3c6551b3f19cbda2e392d661bff65b4b2e02
ANBUSELVAM V <anbuselvamv27@gmail.com>
2026-02-26 18:34:06 +0530

    Update 03_dff_transaction.sv

--- D_flipflop/03_dff_transaction.sv
+++ D_flipflop/03_dff_transaction.sv
@@ -1,12 +1,12 @@
 class transaction;
   
-  bit rst;
+  rand bit rst;
   rand bit d;
   bit q;
   
   function void display(string name);
     $display("------%0s------",name);
-    $display("d = %0d, q = %0d",d,q);
+    $display("rst = %0d,d = %0d, q = %0d",rst,d,q);
   endfunction
   
 endclass
